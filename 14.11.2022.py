c6f86a147bc05b99cd07cd359a0a84fc8b31ac15
Dari <dariaburmist@mail.ru>
2022-11-14 17:19:40 +0300

    Первая версия

--- 14.11.2022.py
+++ 14.11.2022.py
@@ -1,3 +1,12 @@
 import pandas as pd
 df = pd.read_csv("C:/Users/digdepstudent/Documents/Ada/pth_2022_11_14/data.tsv",sep="\t",index_col = "ID")
 print(df.to_string())
+
+df=df.dropna(axis=0)
+print(df.to_string())
+x=df['age'].mean()
+df['age'].fillna(x, inplace= True)
+print(df.to_string())
+print(df.info())
+
+
